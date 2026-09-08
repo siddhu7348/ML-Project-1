ae47c7f69f271f7643c2e43bf05922dfa1c89de1
siddhu7348 <siddhuladde8@gmail.com>
2026-09-09 02:04:13 +0530

    HyperParameter Tuning is done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,8 +49,50 @@ class model_trainer:
                 "Adaboost Classifier": AdaBoostRegressor()
             }
 
+            params={
+                "Random_forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision_tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                     },
+                    "Linear Regression":{},
+                    "K-Neighbors Regressor":{
+                        'n_neighbors': [3,5,7,9,11],
+                        'weights': ['uniform','distance'],
+                        'algorithm': ['auto','ball_tree','kd_tree','brute']
+                    },
+                    "XG-Boost Regressor":{
+                        'learning_rate':[.1,.01,.05,.001],
+                        'n_estimators': [8,16,32,64,128,256]
+                    },
+                    "Catboosting Regressor":{
+                        'depth': [6,8,10],
+                        'learning_rate': [0.01, 0.05, 0.1],
+                        'iterations': [30, 50, 100]
+                    },
+                    "Adaboost Classifier":{
+                        'learning_rate':[.1,.01,0.5,.001],
+                        'loss':['linear','square','exponential'],
+                        'n_estimators': [8,16,32,64,128,256]
+                    }
+                }
+
+
             model_report: dict = evaluate_models(X_train = X_train, X_test = X_test,
-                             y_train = y_train, y_test = y_test, models = models)
+                             y_train = y_train, y_test = y_test, models = models, params = params)
 
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,13 +21,22 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train) #training the model
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv= 5, n_jobs = -1, verbose = 1 , refit = True)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+
+           # model.fit(X_train, y_train) #training the model
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
